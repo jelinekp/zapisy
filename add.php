9d8585bbd9c8a769c4551984139701e75599fa52
Markaos <markaos@email.cz>
2017-05-20 14:32:29 +0200

    Skript pro přidávání písemek se nyní chová stejně jako mobilní API

--- add.php
+++ add.php
@@ -23,7 +23,8 @@ if(isset($_POST["sent"])) {
   $db->exec("CREATE TABLE IF NOT EXISTS authors (author_ID INT AUTO_INCREMENT PRIMARY KEY, `name` VARCHAR(64), `pass` VARCHAR(128), `subjects` VARCHAR(128))");
   $db->exec("CREATE TABLE IF NOT EXISTS exam_files (_ID INT AUTO_INCREMENT PRIMARY KEY, `exam_ID` INT, `name` VARCHAR(128), `type` VARCHAR(8), `data` VARCHAR(256), `author_ID` INT)");
   $query = $db->prepare("INSERT INTO exams (`subject`, `range`, `exam_date`, `notes`, `author`, `grp`) VALUES (?, ?, ?, ?, ?, ?)");
-  $query->execute(array(substr($_POST["subject"], 0, strpos($_POST["subject"], ';')), $_POST["range"], date("Y-m-d", $date), $_POST["notes"], $_POST["author"], substr($_POST["subject"], strpos($_POST["subject"], ';') + 1)));
+  $subname = strpos($_POST["subject"], '(') != 0 ? substr($_POST["subject"], 0, strpos($_POST["subject"], '(')) : substr($_POST["subject"], 0, strpos($_POST["subject"], ';'));
+  $query->execute(array($subname, $_POST["range"], date("Y-m-d", $date), $_POST["notes"], $_POST["author"], substr($_POST["subject"], strpos($_POST["subject"], ';') + 1)));
   header("Location: $srvRoot");
 } else {
   header("Location: $srvRoot");
