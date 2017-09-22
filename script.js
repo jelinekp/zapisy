aafcdf0e1d6b5bef76f34a5499a6428e805c196f
Markaos <marek.cernoch@email.cz>
2017-09-22 16:10:45 +0200

    Odstraněna zbytečná část kódu
    (tak schválně, jestli je Deploy správně nastavený)

--- script.js
+++ script.js
@@ -23,6 +23,5 @@ function load() {
   $(".link-delete").click(function(event) {
     clickId = $(event.target).data("id");
     $("#delete-exam-id").attr("value", $(event.target).data("id"));
-    //window.location = window.location + "/delete.php?id=" + $(event.target).data("id");
   });
 }
