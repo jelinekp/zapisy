6b0339a97bb1a57f491a35b40667885d2875b206
Markaos <marek.cernoch@email.cz>
2017-09-22 16:17:49 +0200

    Další zbytečný komentář
    (Deploy zatím nefunguje)

--- add.php
+++ add.php
@@ -8,15 +8,6 @@ $db = new PDO(
   Config::$db_user, Config::$db_pass
 );
 
-/*if(isset($_POST["action"])) {
-  switch($_POST["action"]) {
-    case 'register':
-      if(isset($_POST['code']) && isset($_POST["salt"])) {
-        $query = $db->prepare("SELECT name FROM codes WHERE ")
-      }
-  }
-}*/
-
 if(isset($_POST["sent"])) {
   $date = DateTime::createFromFormat('!d.m.Y', $_POST["date"])->getTimestamp();
   $db->exec("CREATE TABLE IF NOT EXISTS exams (_ID INT AUTO_INCREMENT PRIMARY KEY, `subject` VARCHAR(64), `range` VARCHAR(128), `exam_date` TIMESTAMP, `notes` VARCHAR(4096), `author` INT, `exam_date_uc` INT, `grp` VARCHAR(32))");
