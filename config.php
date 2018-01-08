193324a257a65c93b4b13415bd991a84ce4c7e3e
Marek Černoch <markaos@email.cz>
2018-01-08 04:09:43 +0100

    Přidána konfigurační volba pro povinné přihlášení

--- config.php
+++ config.php
@@ -4,5 +4,7 @@ class Config {
   public static $db_name = "zapisy";
   public static $db_user = "root";
   public static $db_pass = "root";
+  public static $login_needed = false; // We will have a grace period for
+                                       // honest contributors to update
 }
 ?>
