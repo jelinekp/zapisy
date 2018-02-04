af4be2096d1512b7c499e9f83cf413c3ba20d90d
Marek Černoch <markaos@email.cz>
2018-02-04 17:04:44 +0100

    Opravena spousta chyb

--- assembler.php
+++ assembler.php
@@ -5,11 +5,13 @@ class Assembler {
   private static $parts = null;
   private static $css = array();
   private static $js = array();
+  
+  static $vals = null;
 
   private static function assemble_css() {
     $str = '';
     foreach(static::$css as $file) {
-      $str .= '<link rel="stylesheet" type="text/css" href="' . V::file($file['file']) . '" ' . (isset($file['media']) ? 'media="' . $file['media'] . '" ' : '') . '/>\n';
+      $str .= '<link rel="stylesheet" type="text/css" href="' . V::file($file['file']) . '" ' . (isset($file['media']) ? 'media="' . $file['media'] . '" ' : '') . '/>';
     }
     return $str;
   }
@@ -22,42 +24,50 @@ class Assembler {
     return $str;
   }
 
+  private static function makeParts() {
+    $partlist = array();
+    foreach(json_decode(file_get_contents('parts/parts.json'), true) as $part) {
+      if(isset($part['js'])) {
+        static::$js[] = $part['js'];
+      }
+      if(isset($part['css'])) {
+        foreach($part['css'] as $style) {
+          static::$css[] = $style;
+        }
+      }
+      $partlist[$part['name']] = $part;
+    }
+    static::$parts = $partlist;
+  }
+
   static function assemble(string $part, $vars = array()) {
     if(static::$parts === null) {
-      $partlist = array();
-      foreach(json_decode(file_get_contents('parts/parts.json'), true) as $part) {
-        if(isset($part['js'])) {
-          static::$js[] = $part['js'];
-        }
-        if(isset($part['css'])) {
-          foreach($part['css'] as $style) {
-            static::$css[] = $style;
-          }
-        }
-        $partlist[$part['name']] = $part;
-      }
-      static::$parts = $partlist;
+      static::makeParts();
     }
 
-    if(!isset(static::$parts[$name])) {
+    if(static::$vals == null) {
+      static::$vals = json_decode(file_get_contents('values.json'), true);
+    }
+
+    if(!isset(static::$parts[$part])) {
       return null;
     }
     $output = '';
-    $part = static::$parts[$name];
+    $name = $part;
+    $part = static::$parts[$part];
     if($part['dynamic'] == false) {
       $output = assemble_generic(file_get_contents('parts/' . $name . '.html'), $vars);
     } else {
       require_once('parts/' . $name . '.php');
-      call_user_func('assemble_' . str_replace('-', '_', $name), file_get_contents('parts/' . $name . '.html'), $vars);
+      $output = call_user_func('assemble_' . str_replace('-', '_', $name), file_get_contents('parts/' . $name . '.html'), $vars);
     }
 
-    $vars = json_decode('values.json', true);
-    $output = preg_replace_callback('%\$VAR_([^$]+)\$%', function($match) {
-      if(!isset($vars[$match])) {
-        echo '<span style="display: none;">Couldn\'t find variable ' . $match . '</span>';
+    $output = preg_replace_callback('%\$VAR_([^$]+)\$%', function($matches) {
+      $match = $matches[1];
+      if(!isset(static::$vals[$match])) {
         return '';
       }
-      return $vars[$match];
+      return static::$vals[$match];
     }, $output);
 
     $output = str_replace('$PART_CSS$', static::assemble_css(), $output);
@@ -68,12 +78,17 @@ class Assembler {
 }
 
 function assemble_generic($partData, $vars) {
-  foreach($vars as $key => $value) {
-    $partData = str_replace($key, $value, $partData);
-  }
+  $partData = preg_replace_callback('%\$VAR_([^$]+)\$%', function($matches) use($vars) {
+      $match = $matches[1];
+      if(!isset(Assembler::$vals[$match]) && !isset($vars[$match])) {
+        return '';
+      }
+      return isset($vars[$match]) ? $vars[$match] : Assembler::$vals[$match];
+    }, $partData);
 
   preg_match_all('%\$PART_([^$]+)\$%', $partData, $parts);
-  foreach($parts as $part) {
+  foreach($parts[1] as $part) {
+    if($part == 'CSS' || $part == 'JS') continue;
     $partData = str_replace('$PART_' . $part . '$', Assembler::assemble($part), $partData);
   }
 
