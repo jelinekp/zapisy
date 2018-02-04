3b228c6c8d06b32c25fe566b632279b7311810c9
Marek Černoch <markaos@email.cz>
2018-02-04 15:36:28 +0100

    Přidána implementace dynamických částí

---
+++ parts/list-workbooks.php
@@ -0,0 +1,21 @@
+<?php
+
+function assemble_list_workbooks($markup, $vars) {
+  $output = '';
+  foreach(json_decode('workbooks.json', true) as $workbook) {
+    $part = 'item-workbook';
+    if(isset($sub["deprecated"]) && $sub["deprecated"] == true) {
+      $part = 'item-workbook-deprecated';
+    }
+
+    $output .= Assembler::assemble($part, array(
+      'name'        => $workbook['name'],
+      'icon'        => $workbook['icon'],
+      'url'         => $workbook['url'],
+      'url-archive' => $workbook['archive']
+    ));
+  }
+  return str_replace('$DYN_table$', $output, $markup);
+}
+
+?>
---
+++ parts/modal-add.php
@@ -0,0 +1,11 @@
+<?php
+
+function assemble_modal_add($markup, $vars) {
+  $subjects = Provider::getSubjects();
+  $output = '';
+  foreach($subjects as $subject) {
+    $output .= '<option value="' . $subject['name'] . ';' . $subject['group'] .'">' . $subject['name'] . '</option>\n';
+  }
+  return str_replace('$DYN_subjects$', $output, $markup);
+}
+?>
