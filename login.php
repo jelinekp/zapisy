49eb4b0394e1ca3d0e6cd8f5873f59816b12dc4e
Markaos <marek.cernoch@email.cz>
2018-12-11 22:22:21 +0100

    Přihlášení přesměrovává na správný profil

--- login.php
+++ login.php
@@ -2,6 +2,10 @@
 require_once('util.php');
 if(isset($_POST['code'])) {
   setcookie('code', $_POST['code'], time() + 60*60*24*365*20);
+  $_COOKIE['code'] = $_POST['code'];
+  $user = get_user();
+  header('Location: /?class=' . $user['class']);
+} else {
+  header('Location: /');
 }
-header('Location: /');
 ?>
